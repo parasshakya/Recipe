17c5060d1808a12acd00bc643f156da168dd102a
unknown <parasshakya8085@gmail.com>
2024-08-18 17:24:39 +0545

    initial commit

--- src/components/partials/Navbar.jsx
+++ src/components/partials/Navbar.jsx
@@ -6,7 +6,9 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import { welcomeRoutes } from '../../routes/welcomeRoutes';
 import { useSelector } from 'react-redux';
 import { dashboardRoutes } from '../../routes/dashboardRoutes';
-import { Avatar } from '@mantine/core';
+import { Autocomplete, Avatar } from '@mantine/core';
+import { GetRequest } from '../../plugins/https';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete';
 
 export const Navbar = () => {
 
@@ -15,7 +17,49 @@ export const Navbar = () => {
   const token = useSelector(state => state.authReducer.token) || localStorage.getItem("token");
   const user =  useSelector(state => state.userReducer.profile) || JSON.parse(localStorage.getItem("userData"));
 
-  console.log(user);
+
+  const [recipeSuggestions, setRecipeSuggestions] = useState([]);
+  const[searchTerm, setSearchTerm] = useState("");
+
+
+
+  const fetchSuggestions = async () => {
+    try{
+  
+      const res = await GetRequest("/recipes/search", {
+        params: { query: searchTerm }
+      });
+      
+      
+      if(res.data?.length !== 0){
+        const recipeSuggestions = res.data.map(recipe => ({
+          id: recipe._id, // Use ID as value
+          name: recipe.name // Display recipe name
+        }));
+        console.log("SUGGESTIONS:", recipeSuggestions);
+        setRecipeSuggestions(recipeSuggestions);
+  
+      }else{
+        setRecipeSuggestions([]);
+      }
+  
+    
+    
+   }catch(error){
+    console.error("Error fetching recipes:", error);
+    // Optionally handle the error, e.g., show an error message or clear suggestions
+    setRecipeSuggestions([]);
+  
+  
+   }
+
+
+  }
+
+  
+
+  
+
 
   useEffect(()=>{
     window.addEventListener("resize", ()=>{
@@ -24,6 +68,30 @@ export const Navbar = () => {
      }   
     })
 }, [])
+
+
+  // Handle recipe selection
+  const handleRecipeSelect = (recipe) => {
+    if (recipe) {
+      // Navigate to the recipe detail page using the selected recipe ID
+      navigate(`/recipes/${recipe.id}`);
+    }
+  };
+
+
+  useEffect(() => {
+    if (searchTerm.trim() === '') {
+      setRecipeSuggestions([]);
+    } else {
+      fetchSuggestions(searchTerm);
+    }
+  }, [searchTerm]);
+
+
+  const handleOnSearch = (string) => {
+    setSearchTerm(string);
+  };
+
 
 
 const navigate = useNavigate()
@@ -47,8 +115,14 @@ const navigate = useNavigate()
         }}  className="hamburger-icon md:hidden">
 <IconMenu2  width={"40px"} height={"40px"}/>
         </div>
+   
+
         {
     user  && <div className=' hidden md:flex  md:gap-3   items-center'>
+
+        <ReactSearchAutocomplete    
+ className='w-80' items={recipeSuggestions} placeholder='Search for recipes' onSearch={handleOnSearch} onSelect={handleRecipeSelect}/>
+     
       <Avatar src={`http://localhost:3002/uploads/${user.image}`} className='lg:w-11 lg:h-11 xl:w-12 xl:h-12' />
     <div className='lg:text-[18px] xl:text-xl'>
     {
